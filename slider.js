a85d2a1b59e64f8a2cadddf9948948d6b0677527
wedewede <513011631@qq.com>
2018-02-10 19:42:00 +0800

    轮播1.0 版本

---
+++ slider.js
@@ -0,0 +1,34 @@
+let allButton=$('#buttons>button')
+highLight()
+function highLight(){
+  for(let i=0;i<allButton.length;i++){
+  $(allButton[i]).on('click',function(x){
+    let index=$(x.currentTarget).index()
+    let m=index*-305
+    $('#images').css({
+      'margin-left':m+'px'
+    })
+    allButton.eq(index).addClass('red').siblings().removeClass('red')
+  })
+}
+}
+let n=0
+
+function autoSlide(){
+  n=n+1
+  let count=allButton.length
+  allButton.eq(n%count).trigger('click')
+  
+}
+function setTimer(){
+  return setInterval(function(){
+    autoSlide()
+  },3000)
+}
+let timer = setTimer()
+$('#viewport').on('mouseenter',function(){
+  window.clearInterval(timer)
+})
+$('#viewport').on('mouseleave', function () {
+  timer= setTimer()
+})
