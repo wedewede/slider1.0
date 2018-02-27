218282bdd3538761bb187dfc3bb95709e554688c
wedewede <513011631@qq.com>
2018-02-27 10:58:59 +0800

    修复一个bug

--- slider.js
+++ slider.js
@@ -1,34 +1,34 @@
 let allButton=$('#buttons>button')
 highLight()
 function highLight(){
+  let s=0;
   for(let i=0;i<allButton.length;i++){
-  $(allButton[i]).on('click',function(x){
-    let index=$(x.currentTarget).index()
-    let m=index*-305
-    $('#images').css({
-      'margin-left':m+'px'
-    })
-    allButton.eq(index).addClass('red').siblings().removeClass('red')
-  })
+    $(allButton[i]).on('click',function(x){
+      let index=$(x.currentTarget).index()
+      n=index;
+      let m=index*-305
+      $('#images').css({
+        'margin-left':m+'px'
+      })
+      allButton.eq(index).addClass('red').siblings().removeClass('red');
+    }) 
+  } 
 }
-}
-let n=0
 
-function autoSlide(){
-  n=n+1
-  let count=allButton.length
-  allButton.eq(n%count).trigger('click')
-  
-}
+let n=0
 function setTimer(){
   return setInterval(function(){
-    autoSlide()
-  },1500)
+    n=n+1
+    let count=allButton.length
+    allButton.eq(n%count).trigger('click')
+  },2000)
 }
+
 let timer = setTimer()
-$('#viewport').on('mouseenter',function(){
-  window.clearInterval(timer)
+$('#wrapper').on('mouseleave', function () {
+  timer = setTimer() 
 })
-$('#viewport').on('mouseleave', function () {
-  timer= setTimer()
+
+$('#wrapper').on('mouseenter', function () {
+  window.clearInterval(timer)
 })
